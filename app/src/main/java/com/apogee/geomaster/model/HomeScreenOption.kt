1d719b89920504a7b4069e3f84d248b074a8bb23
AnujDevApogee <anuj.apogee@gmail.com>
2023-07-07 18:24:51 +0530

    working on the adaptor part status:Successfully

---
+++ app/src/main/java/com/apogee/geomaster/model/HomeScreenOption.kt
@@ -0,0 +1,43 @@
+package com.apogee.geomaster.model
+
+import com.apogee.geomaster.R
+
+data class HomeScreenOption(
+    val icon: Int,
+    val title: String,
+    val navId: Int
+) {
+    companion object {
+        val list = listOf(
+            HomeScreenOption(
+                icon = R.drawable.ic_device,
+                title = "Device",
+                navId = -1
+            ),HomeScreenOption(
+                icon = R.drawable.ic_folder,
+                title = "Device",
+                navId = -1
+            ),HomeScreenOption(
+                icon = R.drawable.ic_survey,
+                title = "Survey",
+                navId = -1
+            ),HomeScreenOption(
+                icon = R.drawable.ic_setting,
+                title = "Setting",
+                navId = -1
+            ),HomeScreenOption(
+                icon = R.drawable.ic_notifications,
+                title = "Notify",
+                navId = -1
+            ),HomeScreenOption(
+                icon = R.drawable.ic_notifications,
+                title = "Notify",
+                navId = -1
+            ),HomeScreenOption(
+                icon = R.drawable.ic_notifications,
+                title = "Notify",
+                navId = -1
+            )
+        )
+    }
+}
--- app/src/main/java/com/apogee/geomaster/ui/DeviceFragment.kt
+++ app/src/main/java/com/apogee/geomaster/ui/DeviceFragment.kt
@@ -4,15 +4,32 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.apogee.geomaster.R
+import com.apogee.geomaster.adaptor.HomeScreenAdaptor
 import com.apogee.geomaster.databinding.DeviceLayoutFragmentBinding
+import com.apogee.geomaster.model.HomeScreenOption
+import com.apogee.geomaster.utils.OnItemClickListener
+import com.apogee.geomaster.utils.toastMsg
 
 
-class DeviceFragment :Fragment(R.layout.device_layout_fragment) {
+class DeviceFragment : Fragment(R.layout.device_layout_fragment), OnItemClickListener {
 
     private lateinit var binding: DeviceLayoutFragmentBinding
+    private lateinit var homeScreenAdaptor: HomeScreenAdaptor
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding= DeviceLayoutFragmentBinding.bind(view)
+        binding = DeviceLayoutFragmentBinding.bind(view)
+        recycleView()
+        homeScreenAdaptor.submitList(HomeScreenOption.list)
+    }
 
+    private fun recycleView() {
+        binding.deviceRecycleView.apply {
+            homeScreenAdaptor = HomeScreenAdaptor(this@DeviceFragment)
+            adapter = homeScreenAdaptor
+        }
+    }
+
+    override fun <T> onClickListener(response: T) {
+        activity?.toastMsg("$response")
     }
 }
