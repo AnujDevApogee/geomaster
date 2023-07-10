06ee1b7a8a32c255fa7018e879c5f268a4c917fe
AnujDevApogee <anuj.apogee@gmail.com>
2023-07-10 13:08:54 +0530

    Added the Action Bar Layout status:Successfully

--- app/src/main/java/com/apogee/geomaster/repository/LoginRepository.kt
+++ app/src/main/java/com/apogee/geomaster/repository/LoginRepository.kt
@@ -1,36 +1,10 @@
 package com.apogee.geomaster.repository
 
-class LoginRepository{
+class LoginRepository {
 
-    var responseData=""
-fun myApi(){
-    val RESULTCODE=500
-//    ApiCall().postDataWithoutBody(this,"http://120.138.10.146:8080/BLE_ProjectV6_2/resources/getAllTableRecords/",RESULTCODE)
-}
+    fun myApi() {
 
-//    override fun onResponse(call: Call<*>?, response: Response<*>?, requestCode: Int) {
-//
-//        val responseBody = response!!.body() as ResponseBody?
-//
-//        if (response.isSuccessful) {
-//            if (responseBody != null) {
-//                try {
-//                    responseData= responseBody.string()
-//                    Log.d("TAG", "Flow onResponse: $responseData")
-//                } catch (e: java.lang.Exception) {
-//                    Log.d("TAG", "onResponseException: " + e.message)
-//                }
-//            }
-//        } else {
-//            val strOutput = response.toString()
-////            textViewReceivedDataResponse.text = "Response: $strOutput"
-//            Log.d(ContentValues.TAG, "onResponse: $strOutput")
-//        }
-//    }
-//
-//    override fun onFailure(call: Call<*>?, t: Throwable?, requestCode: Int) {
-//
-//    }
+    }
 
 
 }
