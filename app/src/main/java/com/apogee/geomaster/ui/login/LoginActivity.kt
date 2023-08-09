b5f660e6f702dff57c0039af1747478f2b9f9283
AnujDevApogee <anuj.apogee@gmail.com>
2023-08-09 11:25:19 +0530

    Activity Main And LoginActivity status:Successfully status:Successfully

--- app/src/main/java/com/apogee/geomaster/ui/login/LoginActivity.kt
+++ app/src/main/java/com/apogee/geomaster/ui/login/LoginActivity.kt
@@ -2,16 +2,18 @@ package com.apogee.geomaster.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.apogee.geomaster.R
 import com.apogee.geomaster.databinding.ActivityMainBinding
 import com.apogee.geomaster.ui.HomeScreen
+import com.apogee.geomaster.utils.closeKeyboard
 import com.apogee.geomaster.utils.isInvalidString
+import com.apogee.geomaster.utils.openKeyBoard
 import com.apogee.geomaster.utils.toastMsg
 import com.apogee.geomaster.viewmodel.LoginViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -22,19 +24,19 @@ class LoginActivity : AppCompatActivity() {
     private val myViewModel: LoginViewModel by viewModels()
 
 
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val activityMainBinding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
-        activityMainBinding.viewModel = myViewModel
-        activityMainBinding.lifecycleOwner = this
-        activityMainBinding.executePendingBindings()
+        setContentView(binding.root)
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                myViewModel.msgEvent.collectLatest {  msg ->
+                myViewModel.msgEvent.collectLatest { msg ->
                     if (!isInvalidString(msg)) {
                         toastMsg(msg)
-                        val intent=Intent(this@LoginActivity, HomeScreen::class.java)
+                        val intent = Intent(this@LoginActivity, HomeScreen::class.java)
                         startActivity(intent)
                         finishAffinity()
 
@@ -44,9 +46,28 @@ class LoginActivity : AppCompatActivity() {
         }
 
 
+        val anim=AnimationUtils.loadAnimation(this, R.anim.enter_anim_layout)
+
+        binding.cardLyt.startAnimation(anim)
+
+
+        binding.loginBtn.setOnClickListener {
+            myViewModel.onLoginClicked()
+        }
+
     }
 
 
+    override fun onStart() {
+        super.onStart()
+        openKeyBoard(binding.userNm)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        closeKeyboard(binding.userNm)
+    }
 
 
 }
