27da42ea697a155cd692226764e32b2348da6665
AnujDevApogee <anuj.apogee@gmail.com>
2023-08-10 11:52:31 +0530

    Added Satellite Item Layout status:Successfully

--- app/src/main/java/com/apogee/geomaster/ui/configuration/satellite/SatelliteConfigurationFragment.kt
+++ app/src/main/java/com/apogee/geomaster/ui/configuration/satellite/SatelliteConfigurationFragment.kt
@@ -4,17 +4,32 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.apogee.geomaster.R
+import com.apogee.geomaster.adaptor.SatelliteScreenAdaptor
 import com.apogee.geomaster.databinding.SatelliteConfigurationFragmentBinding
+import com.apogee.geomaster.model.SatelliteModel
 import com.apogee.geomaster.utils.displayActionBar
 
 class SatelliteConfigurationFragment : Fragment(R.layout.satellite_configuration_fragment) {
 
     private lateinit var binding: SatelliteConfigurationFragmentBinding
 
+    private lateinit var adaptor: SatelliteScreenAdaptor
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = SatelliteConfigurationFragmentBinding.bind(view)
-        displayActionBar("Satellite Configuration",binding.actionLayout)
+        displayActionBar("Satellite Configuration", binding.actionLayout)
+        setRecycleView()
 
+    }
+
+    private fun setRecycleView() {
+        binding.recycleView.apply {
+            this@SatelliteConfigurationFragment.adaptor = SatelliteScreenAdaptor {
+
+            }
+            adapter = this@SatelliteConfigurationFragment.adaptor
+            this@SatelliteConfigurationFragment.adaptor.submitList(SatelliteModel.list)
+        }
     }
 }
