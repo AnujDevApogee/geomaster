308d1d60eb388f77d8cf4a848e3739797ae197ef
AnujDevApogee <anuj.apogee@gmail.com>
2023-08-10 12:34:29 +0530

    Added the Device Config Item Layout status:Successfully

--- app/src/main/java/com/apogee/geomaster/ui/configuration/deviceconfig/DeviceConfiguration.kt
+++ app/src/main/java/com/apogee/geomaster/ui/configuration/deviceconfig/DeviceConfiguration.kt
@@ -4,17 +4,32 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.apogee.geomaster.R
+import com.apogee.geomaster.adaptor.DeviceConfigurationAdaptor
 import com.apogee.geomaster.databinding.DeviceConfigLayoutBinding
+import com.apogee.geomaster.model.DeviceWorkMode
 import com.apogee.geomaster.utils.displayActionBar
 
 class DeviceConfiguration : Fragment(R.layout.device_config_layout) {
 
     private lateinit var binding: DeviceConfigLayoutBinding
 
+    private lateinit var adaptor: DeviceConfigurationAdaptor
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DeviceConfigLayoutBinding.bind(view)
         displayActionBar("Device Configuration",binding.actionLayout)
+        setAdaptor()
+    }
+
+    private fun setAdaptor() {
+        binding.recycleView.apply {
+            this@DeviceConfiguration.adaptor= DeviceConfigurationAdaptor {
+
+            }
+            adapter=this@DeviceConfiguration.adaptor
+            this@DeviceConfiguration.adaptor.submitList(DeviceWorkMode.list)
+        }
     }
 
 }
