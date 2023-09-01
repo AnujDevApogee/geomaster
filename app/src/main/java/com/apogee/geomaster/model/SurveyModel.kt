ea38cf359d1314c315670519abf498616ee20e27
rajni <rajni.apogee@gmail.com>
2023-09-01 11:25:09 +0530

    add survey model

---
+++ app/src/main/java/com/apogee/geomaster/model/SurveyModel.kt
@@ -0,0 +1,4 @@
+package com.apogee.geomaster.model
+
+
+class SurveyModel(id: Int, pointName: String,codeName: String, easting: Double, northing : Double, zone: Int,elevation: Double, prefix: String, record_type: String, survey_type: String) {}
