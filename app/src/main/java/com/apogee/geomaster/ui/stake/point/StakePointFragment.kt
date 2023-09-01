452344e0e189da20fb4e257583f56944d7ae0ff7
AnujDevApogee <anuj.apogee@gmail.com>
2023-09-01 12:22:22 +0530

    Added the Stake Point fragment status:Successfully

--- app/src/main/java/com/apogee/geomaster/ui/stake/point/StakePointFragment.kt
+++ app/src/main/java/com/apogee/geomaster/ui/stake/point/StakePointFragment.kt
@@ -3,6 +3,7 @@ package com.apogee.geomaster.ui.stake.point
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.apogee.geomaster.R
 import com.apogee.geomaster.databinding.StakePointFragmentLayoutBinding
@@ -11,11 +12,16 @@ import com.apogee.geomaster.utils.OnSwipeTouchListener
 import com.apogee.geomaster.utils.createLog
 import com.apogee.geomaster.utils.hide
 import com.apogee.geomaster.utils.show
+import com.apogee.geomaster.utils.showMessage
 
 class StakePointFragment: Fragment(R.layout.stake_point_fragment_layout) {
 
     private lateinit var binding: StakePointFragmentLayoutBinding
 
+
+    private val list by lazy {
+        listOf("Point 1", "Point 2", "Point 3", "Point 4", "Point 5", "Point 6", "Point 7")
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -23,18 +29,30 @@ class StakePointFragment: Fragment(R.layout.stake_point_fragment_layout) {
         binding = StakePointFragmentLayoutBinding.bind(view)
         (activity as HomeScreen?)?.hideActionBar()
 
-        binding.cvSliderOption.setOnTouchListener(swipeGesture(binding.infoLayout,false))
-        binding.cvBottom.setOnTouchListener(swipeGesture(binding.layoutDrop,true))
+        binding.cvSliderOption.setOnTouchListener(swipeGesture(binding.infoLayout, false))
+        binding.cvBottom.setOnTouchListener(swipeGesture(binding.layoutDrop, true))
 
+
+        setUpAdaptor()
+
+        binding.listSlidermenu.setOnItemClickListener { _, _, position, id ->
+            showMessage("${binding.listSlidermenu.getItemAtPosition(position)}")
+
+        }
 
     }
 
+    private fun setUpAdaptor() {
+        binding.listSlidermenu.adapter =
+            ArrayAdapter(requireActivity(), android.R.layout.simple_list_item_1, list)
+    }
 
-    private fun swipeGesture(view: View,isBottomView:Boolean): OnSwipeTouchListener {
-        return object: OnSwipeTouchListener(requireActivity()) {
+
+    private fun swipeGesture(view: View, isBottomView: Boolean): OnSwipeTouchListener {
+        return object : OnSwipeTouchListener(requireActivity()) {
             override fun onSwipeRight() {
                 super.onSwipeRight()
-                createLog("TOUCH_MSG","Bottom RIGHT")
+                createLog("TOUCH_MSG", "Bottom RIGHT")
             }
 
             override fun onSwipeLeft() {
