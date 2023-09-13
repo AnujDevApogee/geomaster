3d9769bf54ce601ffd1b6304739a4d91e9cf5813
AnujDevApogee <anuj.apogee@gmail.com>
2023-09-13 09:58:41 +0530

    updated the code form setup-base config

--- app/src/main/java/com/apogee/geomaster/repository/BaseConfigurationRepository.kt
+++ app/src/main/java/com/apogee/geomaster/repository/BaseConfigurationRepository.kt
@@ -2,8 +2,8 @@ package com.apogee.geomaster.repository
 
 import android.app.Application
 import com.apogee.geomaster.utils.ApiResponse
-import com.apogee.geomaster.utils.createLog
 import kotlinx.coroutines.flow.flow
+import java.io.Serializable
 
 class BaseConfigurationRepository(application: Application) {
 
@@ -15,48 +15,42 @@ class BaseConfigurationRepository(application: Application) {
     fun setUpBaseConfig(deviceName: String) = flow {
         emit(ApiResponse.Loading("Loading From DataBase"))
         val data = try {
-            val resultUserRegNo = databaseRepository.getUserRegNo("NAVIK200-1.0")
-            resultUserRegNo?.let { id ->
-                val resultDeviceId = databaseRepository.getDeviceId(id)
-                createLog("DB_INFO", "UserRegNo $id")
-                createLog("DB_INFO", "DeviceId  $resultDeviceId")
-                if (resultDeviceId.isNotEmpty()) {
-                    val makeName =
-                        databaseRepository.getMakeName(resultDeviceId["ManufactureID"]!!.toInt())
-                    createLog("DB_INFO", "MakeName $makeName")
-                    val makeResponse =
-                        databaseRepository.getModuleFinishedId(resultDeviceId["DeviceID"].toString())
-                    createLog(
-                        "DB_INFO",
-                        "ModuleFinished ${
-                            getSubString(
-                                makeResponse.toString(),
-                                1,
-                                makeResponse.toString().length - 1
-                            )
-                        }"
-                    )
-                    if (makeResponse.isNotEmpty()) {
-                        val deviceModule = databaseRepository.getDeviceModule(
-                            getSubString(
-                                makeResponse.toString(),
-                                1,
-                                makeResponse.toString().length - 1
-                            )
-                        )
-                        createLog("DB_INFO","get DeviceModule $deviceModule")
-                        ApiResponse.Success(Pair(makeName, deviceModule))
-                    } else {
-                        ApiResponse.Error("Connect Fetch Response Connection Set up type", null)
-                    }
-                } else {
-                    ApiResponse.Error("Connect Fetch Response DeviceId", null)
-                }
-            } ?: ApiResponse.Error("Connect Fetch Response $deviceName info", null)
+            getBaseResult(deviceName)
         } catch (e: Exception) {
             ApiResponse.Error(e, null)
         }
         emit(data)
+    }
+
+    private suspend fun getBaseResult(deviceName: String): ApiResponse<out Serializable> {
+        val resultUserRegNo = databaseRepository.getUserRegNo(deviceName)
+            ?: return ApiResponse.Error("Connect Fetch Response $deviceName info", null)
+
+        val resultDeviceId = databaseRepository.getDeviceId(resultUserRegNo)
+
+        if (resultDeviceId.isEmpty()) {
+            return ApiResponse.Error("Connect Fetch Response DeviceId", null)
+        }
+
+        val makeName =
+            databaseRepository.getMakeName(resultDeviceId["ManufactureID"]!!.toInt())
+                ?: return ApiResponse.Error("Connect Fetch Manufacture Name", null)
+
+        val makeResponse =
+            databaseRepository.getModuleFinishedId(resultDeviceId["DeviceID"].toString())
+
+        if (makeResponse.isEmpty()) {
+            return ApiResponse.Error("Connect Fetch Response Connection Set up type", null)
+        }
+
+        val makeResLs = getSubString(
+            makeResponse.toString(),
+            1,
+            makeResponse.toString().length - 1
+        )
+        val deviceModule = databaseRepository.getDeviceModule(makeResLs)
+
+        return ApiResponse.Success(Pair(makeName, deviceModule))
     }
 
     private fun getSubString(str: String, from: Int, to: Int): String {
